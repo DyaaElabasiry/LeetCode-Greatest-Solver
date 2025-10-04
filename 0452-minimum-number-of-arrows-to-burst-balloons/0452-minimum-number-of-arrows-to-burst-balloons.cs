963954af04d735041554e095c22756692193e194
Dyaa Elabasiry <52318113+DyaaElabasiry@users.noreply.github.com>
2025-10-04 20:42:40 +0300

    Time: 126 ms (6.57%), Space: 76.1 MB (43.6%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int FindMinArrowShots(int[][] points)
+    {
+        int numberOfCollections = 0;
+        int collectionStart = 0;
+        int i = 0;
+        Array.Sort(points, (a, b) => a[0].CompareTo(b[0]));
+        while (i < points.Length)
+        {
+            numberOfCollections++;
+            collectionStart = i;
+            i++;
+            while (i < points.Length && DoesIntersectWithCollection( collectionStart, i, points))
+            {
+                i++;
+            }
+        }
+        return numberOfCollections; 
+    }
+
+    public bool AreOverlapping(int[] a, int[] b)
+    {
+        return b[0]>=a[0]  && b[0]<=a[1];
+    }
+
+    public bool DoesIntersectWithCollection(int collectionStart, int collectionEnd, int[][] points)
+    {
+        for (int i = collectionStart; i < collectionEnd; i++)
+        {
+            if (!AreOverlapping(points[i], points[collectionEnd]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
