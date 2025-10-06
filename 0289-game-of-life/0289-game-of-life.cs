545880c91c99393f02898bd0754f1a1168490207
Dyaa Elabasiry <52318113+DyaaElabasiry@users.noreply.github.com>
2025-10-07 01:22:33 +0300

    Time: 0 ms (100%), Space: 46.4 MB (69.12%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cs
+++ 0289-game-of-life/0289-game-of-life.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public void GameOfLife(int[][] board) {
-        int[][] boardCopy = new int[board.Length][];
+        //int[][] boardCopy = new int[board.Length][];
         int[][] moves = new int[8][]
         {
             new int[] {-1,-1},
@@ -14,7 +14,7 @@ public class Solution {
         };
         for (int i = 0; i < board.Length; ++i)
         {
-            boardCopy[i] = new int[board[i].Length];
+            //boardCopy[i] = new int[board[i].Length];
             for (int j = 0; j < board[i].Length; ++j)
             {
                 int count = 0;
@@ -23,23 +23,29 @@ public class Solution {
                     if (move[0] + i >= 0 && move[0] +i < board.Length && move[1] + j >= 0 &&
                         move[1] + j < board[0].Length)
                     {
-                        if (board[move[0] + i][move[1]  + j] == 1) count++;
+                        if (board[move[0] + i][move[1]  + j]%2 == 1) count++;
                     }
                 }
 
-                if (count == 3 || (count == 2 && board[i][j] == 1))
+                if (count == 3 || (count == 2 && board[i][j]%2 == 1))
                 {
-                    boardCopy[i][j] = 1;
+                    board[i][j] = board[i][j] == 1 ? 1 : 2;
                 }
                 else
                 {
-                    boardCopy[i][j] = 0;
+                    board[i][j] = board[i][j] == 0 ? 0 : 3;
                 }
                 
             }
                 
         }
 
-        boardCopy.CopyTo(board, 0);
+        for (int i = 0; i < board.Length; ++i)
+        {
+            for (int j = 0; j < board[0].Length; j++)
+            {
+                board[i][j] = board[i][j] == 0 || board[i][j] == 3 ? 0 : 1;
+            }
+        }
     }
 }
