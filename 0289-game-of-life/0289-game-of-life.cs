aeb1d5dcf942d3955c7ff494a4bbc062d8ea9670
Dyaa Elabasiry <52318113+DyaaElabasiry@users.noreply.github.com>
2025-10-06 22:24:51 +0300

    Time: 0 ms (100%), Space: 46.6 MB (25.12%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public void GameOfLife(int[][] board) {
+        int[][] boardCopy = new int[board.Length][];
+        int[][] moves = new int[8][]
+        {
+            new int[] {-1,-1},
+            new int[] {-1,0},
+            new int[] {-1,1},
+            new int[] {0,1},
+            new int[] {1,1},
+            new int[] {1,0},
+            new int[] {1,-1},
+            new int[] {0,-1}
+        };
+        for (int i = 0; i < board.Length; ++i)
+        {
+            boardCopy[i] = new int[board[i].Length];
+            for (int j = 0; j < board[i].Length; ++j)
+            {
+                int count = 0;
+                foreach (var move in moves)
+                {
+                    if (move[0] + i >= 0 && move[0] +i < boardCopy.Length && move[1] + j >= 0 &&
+                        move[1] + j < boardCopy[0].Length)
+                    {
+                        if (board[move[0] + i][move[1]  + j] == 1) count++;
+                    }
+                }
+
+                if (count == 3)
+                {
+                    boardCopy[i][j] = 1;
+                }else if (count == 2 && board[i][j] == 1)
+                {
+                    boardCopy[i][j] = 1;
+                }
+                else
+                {
+                    boardCopy[i][j] = 0;
+                }
+                
+            }
+                
+        }
+
+        boardCopy.CopyTo(board, 0);
+    }
+}
